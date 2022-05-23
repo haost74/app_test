b9b73adf512f1ee06843480eb58e8f7b28db7f7d
haost74 <haost@yandex.ru>
2022-05-23 15:13:27 +0300

    mock

--- test/test.cc
+++ test/test.cc
@@ -1,8 +1,10 @@
 #include <gtest/gtest.h>
 
-#include <gmock/gmock.h>
+#include "gmock/gmock.h"
 
 #include "../src/some_class.cpp"
+
+
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
@@ -16,3 +18,25 @@ TEST(HelloTest, BasicAssertions) {
     some_class sc;
     EXPECT_EQ(40, sc.sum(10, 30) );
  }
+
+ class Turtle {
+  virtual void PenUp() = 0;
+  virtual void PenDown() = 0;
+  virtual void Forward(int distance) = 0;
+  virtual void Turn(int degrees) = 0;
+  virtual void GoTo(int x, int y) = 0;
+  virtual int GetX() const = 0;
+  virtual int GetY() const = 0;
+};
+
+class MockTurtle : public Turtle {
+ public:
+
+  MOCK_METHOD(void, PenUp, (), (override));
+  MOCK_METHOD(void, PenDown, (), (override));
+  MOCK_METHOD(void, Forward, (int distance), (override));
+  MOCK_METHOD(void, Turn, (int degrees), (override));
+  MOCK_METHOD(void, GoTo, (int x, int y), (override));
+  MOCK_METHOD(int, GetX, (), (const, override));
+  MOCK_METHOD(int, GetY, (), (const, override));
+};
